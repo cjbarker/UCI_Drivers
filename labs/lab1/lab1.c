d77f67f848bdc157210e9171f5b1469e0957c4b6
CJ Barker <cjbarker@gmail.com>
2010-11-17 11:37:57 -0600

    lab 1

---
+++ labs/lab1/lab1.c
@@ -0,0 +1,27 @@
+/* LAB 1 - Writing Portable Device Drivers */
+/* poor example of portability              */
+/* fine for tool demonstration              */
+/* Aaron Baranoff                          */
+
+
+void delay(unsigned int val)
+{
+	unsigned int temp;
+	unsigned int temp2;
+
+	for(temp = 0; temp < val; temp++) 
+		for (temp2 = 0; temp2 < val; temp2 ++) ;
+}
+
+int main(void)
+{
+	*((volatile unsigned char *)0x24) = 0xff;
+	while(1) {
+	 	*((volatile unsigned char *)0x25) = 0;
+	 	delay(500);
+	 	*((volatile unsigned char *)0x25) = 0xff;
+	 	delay(500);
+	}
+
+	return(0);
+}
